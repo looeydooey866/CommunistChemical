6ee2e1cd331814ed49d62ff80e948e02f2d47e32
Soyuz-dev <avon2ramesh@gmail.com>
2025-02-23 19:29:47 +0800

    Added orbitals for Atom

--- src/Atom.java
+++ src/Atom.java
@@ -3,7 +3,35 @@ package src;
 public class Atom {
     private int protonNumber;
     private int neutronNumber;
-    private int[] electronShells = new int[7];
+    private static final int S = 2;
+    private static final int P = 6;
+    private static final int D = 10;
+    private static final int F = 14;
+    private int[][] orbitals = {
+            {0},
+            {0, 0},
+            {0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0},
+            {0, 0}
+    };
+    private static final int[][] maxOrbitals = {
+            {2},
+            {2, 6},
+            {2, 6, 10},
+            {2, 6, 10, 14},
+            {2, 6, 10, 14},
+            {2, 6, 10},
+            {2, 6}
+    };
+    //1s
+    //2s       2p
+    //3s       3p
+    //4s    3d 4p
+    //5s    4d 5p
+    //6s 4f 5d 6p
+    //7s 5f 6d 7p
 
-
+    
 }
