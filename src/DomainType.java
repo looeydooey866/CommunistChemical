5e54a92aaef7f80234a625b384af39cd08d66501
Soyuz-dev <avon2ramesh@gmail.com>
2025-05-19 13:37:09 +0800

    Finished up functionality for split() in ElectronDomain.java, made ShellAtom implement Atom, added UNKNOWN type in DomainType.java

--- src/DomainType.java
+++ src/DomainType.java
@@ -4,5 +4,6 @@ public enum DomainType {
     LONE_ELECTRON,
     LONE_PAIR,
     SIGMA_BOND_PAIR,
-    PI_BOND_PAIR
+    PI_BOND_GROUP,
+    UNKNOWN
 }
--- src/ShellAtom.java
+++ src/ShellAtom.java
@@ -1,4 +1,71 @@
 package src;
 
-public class ShellAtom {
+import java.util.ArrayList;
+
+public class ShellAtom implements Atom{
+
+    @Override
+    public void setup(int[][] orbitals) {
+
+    }
+
+    @Override
+    public void fill(int electronNumber) {
+
+    }
+
+    @Override
+    public void ionise(int electronLoss) {
+
+    }
+
+    @Override
+    public void bond(Atom other, int bondOrder, boolean needToRecur) throws CovalentBondException {
+
+    }
+
+    @Override
+    public int[] getValenceShell() {
+        return new int[0];
+    }
+
+    @Override
+    public int[] getMaxValenceShell() {
+        return new int[0];
+    }
+
+    @Override
+    public void addElectronsTo(int[] shell, int[] maxCapacity, int electronsToAdd) {
+
+    }
+
+    @Override
+    public int getMaxCapacityValence() {
+        return 0;
+    }
+
+    @Override
+    public int getNeutronNumber() {
+        return 0;
+    }
+
+    @Override
+    public int getAtomicNumber() {
+        return 0;
+    }
+
+    @Override
+    public ArrayList<Atom> getBondedTo() {
+        return null;
+    }
+
+    @Override
+    public Atom alphaDecay() {
+        return null;
+    }
+
+    @Override
+    public void betaDecay(boolean isPositive) {
+
+    }
 }
